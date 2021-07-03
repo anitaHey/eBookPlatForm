b5c6c4bf1ee7fe5c4f02ca2de11aca2e868770e7
anitaHey <jiaa.anita@gmail.com>
2021-07-03 20:00:34 +0800

    fix text

--- src/app/components/insert-object/insert-text/insert-text.component.ts
+++ src/app/components/insert-object/insert-text/insert-text.component.ts
@@ -10,9 +10,13 @@ import { FontManagementService } from 'src/app/services/font-management.service'
   styleUrls: ['./insert-text.component.css']
 })
 export class InsertTextComponent extends BasicObjComponent implements OnInit, AfterViewInit {
-  model = '';
+  model = '123';
   input_state: number = 1;
-
+  currentRange: Range = document.createRange();
+  caretPosition!: any;
+  rangeContainer: Array<Node> = [];
+  rangeNum: number = 0;
+  isEnter: boolean = false;
 
   constructor(private injector: Injector, private applicationRef: ApplicationRef, private CFR: ComponentFactoryResolver, private node_element: ElementRef
     , private node_renderer: Renderer2, private node_paperManagementService: PaperManagementService, private fontService: FontManagementService) {
@@ -40,6 +44,10 @@ export class InsertTextComponent extends BasicObjComponent implements OnInit, Af
       event.stopPropagation();
     });
 
+    this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'mouseup', (event) => {
+      // this.getCaretCharacterOffsetWithin(this.node_element.nativeElement.querySelector(".text_node"));
+    });
+
     this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'compositionstart', (event) => {
       this.input_state = 0;
     });
@@ -48,59 +56,133 @@ export class InsertTextComponent extends BasicObjComponent implements OnInit, Af
       this.input_state = 1;
     });
 
-    this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'selectstart', (event) => {
-      // console.log("aaa");
+    this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'selectionchange', (event) => {
+      // console.log(window.getSelection()?.getRangeAt(0));
+      // console.log(window.getSelection());
+      // this.getCaretCharacterOffsetWithin(this.node_element.nativeElement.querySelector(".text_node"));
+    });
+
+    this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'keypress', (event) => {
+      if (this.input_state == 1 && event.keyCode == 13) {
+        this.isEnter = true;
+        // event.preventDefault();
+        // event.stopPropagation();
+      }
     });
 
     this.node_renderer.listen(this.node_element.nativeElement.querySelector(".text_node"), 'keyup', (event) => {
       if (this.input_state == 1 && event.keyCode != 37 && event.keyCode != 38 && event.keyCode != 39 && event.keyCode != 40 && event.keyCode != 229) {
-        // console.log((event.target as Element).removeChild((event.target as Element).childNodes[0]));
+        let num = this.getCaretCharacterOffsetWithin(this.node_element.nativeElement.querySelector(".text_node"));
         let node = (event.target as Element);
-        console.log(event.target);
-        var node_child = Array.from(node.childNodes);
-        // node.innerHTML = "";
-        for(let tem of node_child) {
-          if(tem.nodeName.includes("text") && tem.nodeValue!= null) {
-            for (var n of tem.nodeValue) this.createSpan(n, tem);
-          } else if(tem.nodeName.includes("DIV")) {
-            let div_child = Array.from(tem.childNodes);
-            for(let div_tem of div_child) {
-              if(div_tem.nodeName.includes("text") && div_tem.nodeValue!= null)
-                for (var n of div_tem.nodeValue) this.createSpan(n, div_tem);
-            }
-          }
-        }
+        let child = Array.from(node.childNodes);
+
+        node.innerHTML = "";
+        this.addNode(child, node);
+
+        this.setCurrentOffset(node, num);
       }
     });
   }
 
-  createSpan(text: string, childNode: ChildNode) {
-    const caretPosition = window.getSelection()!.getRangeAt(0);
-    const range = document.createRange();
+  addNode(child: ChildNode[], parent: Element) {
+    let new_div = document.createElement("div");
+    let add = false;
+    for (let tem of child) {
+      if (tem.nodeName.includes("DIV")) {
+        if (add) {
+          parent.appendChild(new_div);
+          add = false;
+        }
+        this.addNode(Array.from(tem.childNodes), parent);
+      } else {
+        add = true;
+        if (tem.nodeName.includes("text") && tem.nodeValue != null)
+          for (var n of tem.nodeValue) this.createSpan(n, new_div);
+        else if (tem.nodeName == "SPAN") {
+          if ((tem as HTMLElement).innerText.length >= 1)
+            for (var n of (tem as HTMLElement).innerText) this.createSpan(n, new_div);
+          else new_div.appendChild(tem);
+        }
+      }
+    }
 
-    range.setStart(
-      caretPosition.commonAncestorContainer,
-      caretPosition.startOffset
-    );
-    range.setEnd(
-      caretPosition.commonAncestorContainer,
-      caretPosition.endOffset
-    );
-    const child = this.node_renderer.createElement("app-insert-word");
-    const factory = this.CFR.resolveComponentFactory(InsertWordComponent);
-    const cmp = factory.create(this.injector, [], child);
+    if (add) {
+      parent.appendChild(new_div);
+      add = false;
+    }
 
-    this.applicationRef.attachView(cmp.hostView);
-    cmp.instance.text = text;
-    cmp.instance.fontColor = this.fontService.getCurrentColor();
-    cmp.instance.fontFamily = this.fontService.getCurrentFamily();
-    cmp.instance.fontSize = this.fontService.getCurrentSize();
+  }
 
-    childNode.parentNode?.replaceChild(child, childNode);
+  createSpan(text: string, parentNode: ChildNode) {
+    let child = document.createElement("span");
+    child.innerHTML = text;
+    child.style.cssText = "font-family: " + this.fontService.getCurrentFamily() + ";color: " + this.fontService.getCurrentColor() + ";font-size: " + this.fontService.getCurrentSize() + "px;";
 
-    console.log(range.endOffset);
-    console.log(caretPosition.endOffset);
-    caretPosition.setStart(range.endContainer, range.endOffset);
-    caretPosition.setEnd(caretPosition.endContainer, caretPosition.endOffset);
+    parentNode.appendChild(child);
+  }
+
+  getCaretCharacterOffsetWithin(element: Element) {
+    let start = 0;
+    let end = 0;
+
+    if (window.getSelection() != null) {
+      let sel = window.getSelection();
+      if (sel != null && sel.rangeCount > 0) {
+        let range = window.getSelection()!.getRangeAt(0);
+        this.caretPosition = window.getSelection()?.getRangeAt(0);
+
+        let preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(element);
+        preCaretRange.setEnd(range.startContainer, range.startOffset);
+        start = preCaretRange.toString().length;
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+        end = preCaretRange.toString().length;
+
+        this.rangeContainer = [element, range.endContainer];
+        this.rangeNum = start;
+      }
+    }
+
+    return start;
+  }
+
+  setCurrentOffset(parent: Element, startNum: number) {
+    if (window.getSelection() != null) {
+      let container = this.getNewInsertNode(parent, 0, startNum - 1);
+      let start = 1;
+      let end = 1;
+
+      if (this.isEnter) {
+        end = 0;
+        container = (container as Element).parentElement?.nextElementSibling;
+        this.isEnter = false;
+      }
+
+      this.caretPosition.selectNodeContents(parent);
+      this.caretPosition.setStart(container, start);
+      this.caretPosition.setEnd(container, end);
+      window.getSelection()?.removeAllRanges();
+      window.getSelection()?.addRange(this.caretPosition);
+    }
+  }
+
+  getNewInsertNode(node: Element, count: number, num: number): any {
+    let list = node.childNodes;
+    let node_child = Array.from(list);
+console.log(num);
+    for (let tem of node_child) {
+      // console.log(tem.nodeName);
+      if (tem.nodeName != "DIV" && count == num) return tem;
+      else if (tem.nodeName == "SPAN") count++;
+      else if (tem.nodeName == "DIV") {
+        // console.log(tem.childNodes.length);
+        // console.log((num-count));
+        if (tem.childNodes.length >= (num - count)) {
+
+          return this.getNewInsertNode((tem as Element), count, num);
+        }
+        else count += tem.childNodes.length;
+      }
+    }
   }
 }
