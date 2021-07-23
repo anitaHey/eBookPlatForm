6ff02eb945a6ed59221b1221bb9cf6458f5b9486
anitaHey <jiaa.anita@gmail.com>
2021-07-23 15:49:18 +0800

    Fix text family change!!

--- src/app/area/document-main-style-area/document-main-style-area.component.ts
+++ src/app/area/document-main-style-area/document-main-style-area.component.ts
@@ -42,24 +42,62 @@ export class DocumentMainStyleAreaComponent implements OnInit {
         let font = this.font_family_list[num];
         let node_list = this.selectManagement.getSelectedContent();
         let length = node_list.length;
-        var node = document.createElement('span');
-
+        let node = document.createElement('span');
+        let count = 0;
         for (let i = 0; i < length; i++) {
-          if((node_list[0] as HTMLSpanElement).nodeName == "DIV") {
-            // console.log((node_list[0] as HTMLSpanElement).childNodes);
+          if((node_list[count] as HTMLSpanElement).nodeName == "DIV") {
+            let new_div = document.createElement('div');
+            let child = Array.from(node_list[count].childNodes);
+            for(let n = 0; n < child.length; n++) {
+              (child[n] as HTMLSpanElement).style.fontFamily = font;
+              new_div.appendChild(child[n] as HTMLSpanElement);
+            }
+            node.appendChild(new_div);
+            count++;
           } else {
-            (node_list[0] as HTMLSpanElement).style.fontFamily = font;
-            node.appendChild(node_list[0]);
+            (node_list[count] as HTMLSpanElement).style.fontFamily = font;
+            node.appendChild(node_list[count]);
           }
         }
+
         let range = (this.selectManagement.getSelectedRange() as unknown as Range);
         range.deleteContents();
 
-        console.log(this.selectManagement.getSelectTextIsNewLine());
+        if(!this.selectManagement.getSelectTextDiv()[0].isSameNode(this.selectManagement.getSelectTextDiv()[1])) {
+          if(!this.selectManagement.getSelectTextIsNewLine()[0]) {
+            range.selectNodeContents(this.selectManagement.getSelectTextDiv()[0]);
+            range.setStart(this.selectManagement.getSelectTextDiv()[0], this.selectManagement.getSelectTextDiv()[0].childNodes.length -1);
+            range.setEnd(this.selectManagement.getSelectTextDiv()[0], this.selectManagement.getSelectTextDiv()[0].childNodes.length -1);
 
+            let insert_tem = node.childNodes[0];
+            range.insertNode(insert_tem);
+            insert_tem.replaceWith(...Array.from(insert_tem.childNodes));
+
+          }
+
+          if(!this.selectManagement.getSelectTextIsNewLine()[1]) {
+            range.selectNodeContents(this.selectManagement.getSelectTextDiv()[1]);
+            range.setStart(this.selectManagement.getSelectTextDiv()[1], 0);
+            range.setEnd(this.selectManagement.getSelectTextDiv()[1], 0);
+
+            let insert_tem = node.childNodes[node.childNodes.length-1];
+            range.insertNode(insert_tem);
+            insert_tem.replaceWith(...Array.from(insert_tem.childNodes));
+          }
+
+          let array = Array.from((this.selectManagement.getSelectTextDiv()[0].parentElement as unknown as Element).childNodes);
+          let first_num = 0;
+          for(let num = 0; num < array.length; num++)
+            if(array[num].isSameNode(this.selectManagement.getSelectTextDiv()[0])) first_num = num + 1;
+
+          range.selectNodeContents(array[first_num]);
+          range.setStart(array[first_num], 0);
+          range.setEnd(array[first_num], 0);
+        } else {
+          range = (this.selectManagement.getSelectedRange() as unknown as Range);
+        }
 
         range.insertNode(node);
-
         node.replaceWith(...Array.from(node.children));
     }
   }
